7d33e5372d7da98487d8541d34f4df2db7292a4e
aurorakuqi2024 <kuqiaurora17@gmail.com>
2024-11-19 16:14:43 +0100

    ushtrimi nje

---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ echo "Emri im eshte Aurora"
+ echo "data sot eshte:" 
+ date "+%Y-%m-%d %H:%M:%S"
+ echo -e "\nenter the path"
+ read the_path
+ echo -e "\nyoutpath has the following files and folders"
+ ls $the_path
+
